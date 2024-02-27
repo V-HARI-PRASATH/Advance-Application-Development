f6029c4f59a49f6e45a6d0605db8bb2704ccd285
V-HARI-PRASATH <113664554+V-HARI-PRASATH@users.noreply.github.com>
2024-02-27 16:31:21 +0530

    backend first commit

---
+++ agrofundxbackend/src/main/java/com/appdevteam/agrofundxbackend/entity/Loan.java
@@ -0,0 +1,28 @@
+package com.appdevteam.agrofundxbackend.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class Loan {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    int id;
+    String name;
+    String description;
+    float rateOfIntrest;
+    int minLoanAmt;
+    int maxLoanAmt;
+    int minLoanPeriod;
+    int maxLoanPeriod;
+}
